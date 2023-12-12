1de37078370e60bc308d40d4d7f3941fbf6d54ce
JAKRIDNER <153582970+JAKRIDNER@users.noreply.github.com>
2023-12-11 23:29:31 -0600

    usermodel.js

---
+++ usermodel.js
@@ -0,0 +1,12 @@
+// server/models/UserModel.js
+
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', {
+    username: String,
+    password: String,
+});
+
+module.exports = User;
+
+
